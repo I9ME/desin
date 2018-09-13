2f9363443ceb5319b88b7823a49600961dd07884
DIOGO SOARES <diogo@i9me.com.br>
2018-09-12 23:58:40 -0300

    Feature de Cadastro de Eventos, finalizada

---
+++ template-parts/eventos/eventos-thankyou.php
@@ -0,0 +1,20 @@
+<?php
+	$value_1 = $_POST['value_1'];
+?>
+
+	<div class="Section-subSection u-positionRelative u-sizeFull u-paddingVertical--inter--px">
+		<header class="Section-subSection-header u-marginBottom--inter u-alignCenter">
+			<h4 class="Section-subSection-header-title">
+				Obrigado pela sua colaboração!
+			</h4>
+
+		</header>
+
+		<div class="Section-subSection-content">
+			<p class="u-alignCenter">Lorem Ipsum desc é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
+			<p class="u-alignCenter"><strong>Obrigado!</strong>.
+			<div class="u-marginTop--inter">
+				
+			</div>
+		</div>
+	</div>
