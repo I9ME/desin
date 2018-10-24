14b760b45dedddcdbe5a71444063f6d1f79b2f10
DIOGO SOARES <diogo@i9me.com.br>
2018-10-24 12:00:10 -0300

    implementações no branding

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 	<header id="masthead" class="Site-header u-alignCenter u-paddingVertical u-positionAbsolute u-sizeFull u-displayFlex is-animating u-zIndex20">
 		
 		<!-- Logo do site -->
-		<div class="Site-header-branding u-size4of24--px u-displayInlineFlex u-flexAlignItemsCenter u-positionRelative  u-paddingHorizontal--inter--half"> 
+		<div class="Site-header-branding u-displayInlineFlex u-flexAlignItemsCenter u-positionRelative  u-paddingHorizontal--inter--half"> 
 			<?php  get_template_part('template-parts/header/header','branding');?>
 		</div>
 
